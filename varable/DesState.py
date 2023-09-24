b792d8b23970587034569ceda6df0fa036b72e0e
BrivaelFer <77669345+BrivaelFer@users.noreply.github.com>
2023-09-24 15:00:21 +0200

    lancer %

--- varable/DesState.py
+++ varable/DesState.py
@@ -11,12 +11,11 @@ def Tirage(tail, nt):
     while i < nt:
         n = random.randint(1, tail)
         t[n - 1] += 1
-        print("Jet " + str(i + 1) + " : " + str(n))
         i += 1
         
     return t
 
-def State(tirage):
+def Grah(tirage):
     
     for i, nv in enumerate(tirage):
         x = ""
@@ -24,5 +23,15 @@ def State(tirage):
             x += "X"
         print(str(i + 1) + "|" +x)
         
+def PourCent(tirage):
+    ntl = 0
+    for t in tirage:
+        ntl += t
+    for i, t in enumerate(tirage):
+        prc = (t / ntl) * 100
+        print("Le resulta " + str(i + 1) + " est sorti sur " 
+              + str(prc) + "%" + " des lancer soit " + str(t) + " fois")
 
-State(Tirage(8, 20))
+#Grah(Tirage(8, 20))
+
+PourCent(Tirage(6, 800))
